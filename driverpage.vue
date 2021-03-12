f8048e44abb9a3413f5a4af2a8bbb3333a1b9cb1
Hari narayanan <harinarayanan273857@gmail.com>
2021-03-12 12:52:23 +0530

    initial commit

---
+++ driverpage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <!--Navbar-->
+    <v-app-bar dense color="black" dark app>
+      <v-toolbar-title class="font-weight-medium">SHUTTLE</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn dark icon v-bind="attrs" v-on="on">
+            <v-icon color="white">mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click.prevent="logout"> Logout </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <!--Navbar ends-->
+    <v-snackbar rounded="xl" text top dark v-model="snackbar" timeout="5000"
+      ><span class="white--text mx-15">Customer Destination Details</span>
+      <v-spacer></v-spacer>
+      <v-layout class="my-2" row wrap>
+        <v-flex lg2></v-flex>
+        <v-flex class="mx-5">
+          <v-btn small color="green" text>Accept</v-btn>
+          <v-btn small color="red" text>Reject</v-btn></v-flex
+        >
+      </v-layout>
+    </v-snackbar>
+  </v-app>
+</template>
+<script>
+export default {
+  name: "Dpage",
+  data: () => {
+    return { snackbar: true };
+  },
+  methods: {
+    logout() {
+      this.$session.destroy();
+      this.$router.push({ name: "Login" });
+    },
+  },
+};
+</script>
